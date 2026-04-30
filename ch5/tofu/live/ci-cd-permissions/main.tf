e384cac2dbef5240422e1ae36bf85de3c5f2a6bb
Filip Rysavy (Foxyra Com) <fil@example.com>
2026-04-30 15:24:48 +0200

    Setup OICD

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -20,13 +20,13 @@ module "iam_roles" {
   enable_iam_role_for_testing = true                            
 
   # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  github_repo      = "firyx/devops-book" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
   enable_iam_role_for_apply = true                                
 
   # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  tofu_state_bucket         = "fundamentals-of-devops-tofu-state-firyx" 
+  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state-firyx" 
 }
